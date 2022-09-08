9b9d7c4f66c91d70d0734e8bc319cbba79159ca0
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-09-08 21:35:30 +0530

    import java.util.Scanner; class Pattern2 {
    public static void main(String args[])
    {
    Scanner sc=new Scanner(System.in);
    int r;
    r=sc.nextInt();
    
    for(int i=1;i<=r;i++)
    {
     for(int j=1;j<=r;j++)
    {
    System.out.print(i+" ");
    }
    System.out.println();
    }
    }
    }

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Pattern2
+{
+
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int r;
+r=sc.nextInt();
+
+for(int i=1;i<=r;i++)
+{
+ for(int j=1;j<=r;j++)
+{
+System.out.print(i+" ");
+}
+System.out.println();
+}
+}
+}
