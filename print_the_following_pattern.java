bc182d80e409d582222950643c9c6794795e6a7b
navyasriharsha <84239608+navyasriharsha@users.noreply.github.com>
2022-09-08 21:53:02 +0530

    import java.util.Scanner; class Main {
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		Scanner sc=new Scanner(System.in);
    		int r,k=0;
    		r=sc.nextInt();
    		
    		for(int i=1;i<=r;i++)			// rows 
    		{   
    			for(int j=1;j<=r;j++)		// cols 
    			{
    				System.out.print((char)(64+i)+" ");
    			}
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
- class Main
-{
+class Main {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int r;
+		int r,k=0;
 		r=sc.nextInt();
 		
 		for(int i=1;i<=r;i++)			// rows 
 		{   
-			for(int j=1;j<=i;j++)		// cols 
+			for(int j=1;j<=r;j++)		// cols 
 			{
-				System.out.print(j);
+				System.out.print((char)(64+i)+" ");
 			}
 			System.out.println();
 		}
